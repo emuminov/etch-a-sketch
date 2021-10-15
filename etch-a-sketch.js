ca5bebdf5af6a5b5e6d15ccaba93213dbc978bd7
pdlmn <djeinwings@gmail.com>
2021-10-16 00:57:47 +0300

    Added color and size pickers

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,24 @@
-const chosenColor = 'yellow';
+const drawingAreaSizePicker = document.querySelector('#drawing-area-size-picker');
+const colorPicker = document.querySelector('#color-picker');
 const numberOfDivs = ``;
+let labelForDrawingAreaSize = document.querySelector('[for=drawing-area-size-picker]');
+let chosenColor = document.querySelector('#color-picker').value;
 
-function fillDrawingPageWithDivs(numberOfDivs) {
+function generatePixelDivs(numberOfDivs) {
   let drawingAreaDiv = document.querySelector('#drawing-area');
   let sizeOfPixel  = 500 / numberOfDivs + 'px';
 
   for (let i = 0; i < (numberOfDivs ** 2) ; i++) {
     let drawingAreaPixel = document.createElement('div');
     drawingAreaPixel.id = 'div' + i;
-    drawingAreaPixel.style.cssText = `background-color: black;
+    drawingAreaPixel.style.cssText = `background-color: white;
       width: ${sizeOfPixel};
       height: ${sizeOfPixel}`
     drawingAreaPixel.addEventListener('mouseover', changePixelColor);
 
+    refreshInputLabel();
     drawingAreaDiv.appendChild(drawingAreaPixel);
-    console.log(i);
+    // console.log(i);
   }
 }
 
@@ -22,4 +26,15 @@ function changePixelColor(e) {
   e.target.style.backgroundColor = chosenColor;
 }
 
-fillDrawingPageWithDivs(16);
+function changeColor(e) {
+  chosenColor = e.target.value;
+}
+
+function refreshInputLabel() {
+  let valueOfInput = drawingAreaSizePicker.value;
+  labelForDrawingAreaSize.textContent = `${valueOfInput}x${valueOfInput}`;
+}
+
+drawingAreaSizePicker.addEventListener('input', refreshInputLabel);
+colorPicker.addEventListener('input', changeColor);
+generatePixelDivs(16);
